61bc0e4c32d26f01668db959929bf8fcae400bdb
Tatsiana Slabodchykava <t.slabodchykava@hackspace.by>
2020-07-25 14:47:51 +0300

    show todos

---
+++ main.js
@@ -0,0 +1,50 @@
+const todoStatuses = {
+	toDo: {
+		name: 'toDo',
+		displayName: 'To Do',
+	},
+	inProgress: {
+		name: 'inProgress',
+		displayName: 'In Progress',
+	},
+	done: {
+		name: 'done',
+		displayName: 'Done',
+	},
+};
+
+const todos = [{
+	id: 1,
+	text: 'Buy a cat',
+	status: todoStatuses.done.name,
+}, {
+	id: 7,
+	text: 'Buy 39 more',
+	status: todoStatuses.inProgress.name,
+}, {
+	id: 153,
+	text: 'Live happily',
+	status: todoStatuses.toDo.name,
+}];
+
+function createTodoElement({ id, text, status }) {
+	const todoElement = document.createElement('li');
+	todoElement.id = id;
+
+	const textNode = document.createTextNode(`${text}: ${todoStatuses[status].displayName}`);
+	todoElement.appendChild(textNode);
+
+	return todoElement;
+}
+
+function createTodoList() {
+	const todosDom = document.getElementById('todos');
+	const todosListDom = document.createElement('ul');
+
+	todos.forEach(todo => {
+		const todoElement = createTodoElement(todo);
+		todosListDom.appendChild(todoElement);
+	});
+
+	todosDom.appendChild(todosListDom);
+}
